2fe81618438f4069f91bf86beff9b809565ffff8
mrsinamaleki <sina67631@gmail.com>
2025-04-03 12:19:29 +0330

    feat: session2(model testing)

--- bookfront/models.py
+++ bookfront/models.py
@@ -1,15 +1,46 @@
 from django.db import models
-
 from account.models import CustomUser
-
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+# class Book(models.Model):
+#     title = models.CharField(max_length=100)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#     book_owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f"Book: {self.title}"
 
-    book_owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f"Book: {self.title}"
+        return self.name
+
+    def get_book_count(self):
+        return 0
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(2)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    categories = models.ManyToManyField(Category, related_name="books")
+    published_date = models.DateField()
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f"/books/{self.title}/"
 
--- bookfront/tests.py
+++ bookfront/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from rest_framework.exceptions import ValidationError
+
+from bookfront.models import Book, Author, Category
+
+class AuthorModelTest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name="Hellia")
+
+    def test_author_name_uniqueness(self):
+        """This method ensures that the name of an author is unique"""
+        with self.assertRaises(Exception):
+            Author.objects.create(name="Hellia")
+
+
+    def test_get_book_count(self):
+        self.assertEqual(self.author.get_book_count(), 0)
+
+        Book.objects.create(author=self.author, title="Test", published_date=date(2000, 6, 26))
+
+        self.assertEqual(self.author.get_book_count(), 1)
+
+
+class BookModelTest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name="Sanaz")
+        self.category = Category.objects.create(name="Fantasy")
+
+    def test_book_title_min_length(self):
+        book = Book.objects.create(title="A", author=self.author, published_date=date(2000, 6, 26))
+        with self.assertRaises(ValidationError):
+            book.full_clean()
+
+
+    def test_get_absolute_url(self):
+        book = Book.objects.create(title="booktesttitle", author=self.author, published_date=date(2000, 6, 26))
+        expected_url = f"/books/{book.id}/"
+        self.assertEqual(book.get_absolute_url(), expected_url)
+
+
+class RelationshipTest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name="Parisa")
+        self.category_fantesy = Category.objects.create(name="Fantasy1")
+        self.category_advanture = Category.objects.create(name="Advanture")
+
+        self.book = Book.objects.create(title= "Art of war", author=self.author, published_date=date(2000,3,11))
+
+        # many to many relationship
+        self.book.categories.add(self.category_fantesy, self.category_advanture)
+
+
+    def test_book_author_relation(self):
+        self.assertEqual(self.book.author.name, "Parisa")
+
+    def test_book_category_relationship(self):
+        self.assertEqual(self.book.categories.count(), 2)
+        self.assertIn(self.category_fantesy, self.book.categories.all())
+        self.assertIn(self.category_advanture, self.book.categories.all())
